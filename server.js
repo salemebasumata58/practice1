50423178a18ae851024476794266286458003008
salemes58 <sbasumatas58@gmail.com>
2023-01-04 19:18:11 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const jsonserver = require("json-server");
+const server = jsonserver.create();
+const router = jsonserver.router("db.json");
+const middlewares = jsonserver.defaults();
+const port = process.env.PORT || 3000;
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
